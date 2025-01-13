a22d07337554865e037f92967e243373290fd3e5
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-01-13 19:06:27 +0100

    feat: footer cta

--- contents/Footer.jsx
+++ contents/Footer.jsx
@@ -1,7 +1,32 @@
 const Footer = () => {
   return (
     <footer>
-      F
+      <div className="cta">
+        <div className="container">
+          <ul>
+            <li>
+              <img src="../src/assets/img/buy-comics-digital-comics.png" alt="" />
+              <a href="">digital comics</a>
+            </li>
+            <li>
+              <img src="../src/assets/img/buy-comics-merchandise.png" alt="" />
+              <a href="">dc merchandise</a>
+            </li>
+            <li>
+              <img src="../src/assets/img/buy-comics-subscriptions.png" alt="" />
+              <a href="">subscription</a>
+            </li>
+            <li>
+              <img src="../src/assets/img/buy-comics-shop-locator.png" alt="" />
+              <a href="">comic shop locator</a>
+            </li>
+            <li>
+              <img src="../src/assets/img/buy-dc-power-visa.svg" alt="" />
+              <a href="">dc power visa</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </footer>
   )
 }
