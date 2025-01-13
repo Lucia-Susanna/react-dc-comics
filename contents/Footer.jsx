a9dd751979761388e399b237b21052b6bd3acab0
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-01-13 23:05:56 +0100

    feat: footer nav-bar

--- contents/Footer.jsx
+++ contents/Footer.jsx
@@ -27,6 +27,56 @@ const Footer = () => {
           </ul>
         </div>
       </div>
+      <div className="nav">
+        <div className="container">
+          <div className="col">
+            <ul>
+              <h4>dc comics</h4>
+              <li><a href="">Characters</a></li>
+              <li><a href="">Comics</a></li>
+              <li><a href="">Movies</a></li>
+              <li><a href="">TV</a></li>
+              <li><a href="">Games</a></li>
+              <li><a href="">Videos</a></li>
+              <li><a href="">News</a></li>
+            </ul>
+            <ul>
+              <h4>shop</h4>
+              <li><a href="">Shop DC</a></li>
+              <li><a href="">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+          <div className="col">
+            <ul>
+              <h4>DC</h4>
+              <li><a href="">Terms Of Use</a></li>
+              <li><a href="">Privacy polici (New)</a></li>
+              <li><a href="">Ad Choices</a></li>
+              <li><a href="">Advertising</a></li>
+              <li><a href="">Jobs</a></li>
+              <li><a href="">Subscription</a></li>
+              <li><a href="">Talent Workshops</a></li>
+              <li><a href="">CPSC Certificates</a></li>
+              <li><a href="">Ratings</a></li>
+              <li><a href="">Shop Help</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div className="col">
+            <ul>
+              <h4>sites</h4>
+              <li><a href="">DC</a></li>
+              <li><a href="">MAD Magazine</a></li>
+              <li><a href="">DC Kids</a></li>
+              <li><a href="">DC Universe</a></li>
+              <li><a href="">DC Power Visa</a></li>
+            </ul>
+          </div>
+          <div className="logo">
+            <img src="../src/assets/img/dc-logo-bg.png" alt="" />
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
