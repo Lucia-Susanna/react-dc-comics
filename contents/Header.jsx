189182888ca90a762434f07c7c045dc0b5151532
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-01-13 17:18:05 +0100

    feat: header

--- contents/Header.jsx
+++ contents/Header.jsx
@@ -1,7 +1,45 @@
 const Header = () => {
   return (
     <header>
-      H
+      <div className="container">
+        <figure>
+          <img src="/dc-logo.png" alt="logo" />
+        </figure>
+        <nav>
+          <ul>
+            <li>
+              <a href="">characters</a>
+            </li>
+            <li>
+              <a href="">comics</a>
+            </li>
+            <li>
+              <a href="">movies</a>
+            </li>
+            <li>
+              <a href="">tv</a>
+            </li>
+            <li>
+              <a href="">games</a>
+            </li>
+            <li>
+              <a href="">collectibles</a>
+            </li>
+            <li>
+              <a href="">videos</a>
+            </li>
+            <li>
+              <a href="">fans</a>
+            </li>
+            <li>
+              <a href="">news</a>
+            </li>
+            <li>
+              <a href="">shop</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
